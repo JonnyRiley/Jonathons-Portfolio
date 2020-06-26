58fac381726f0003659404df061f0c164088fb9a
JonnyRiley <56361035+JonnyRiley@users.noreply.github.com>
2020-06-26 16:21:32 +0100

    Updates

---
+++ precache-manifest.5038bd18ce8dfc56fc9829514bf01b2f.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc782df1cd0f7088e43d7c79a296a80",
+    "url": "/Jonathons-Portfolio/index.html"
+  },
+  {
+    "revision": "8e1cbec9ebd37524265a",
+    "url": "/Jonathons-Portfolio/static/css/main.f27fbb73.chunk.css"
+  },
+  {
+    "revision": "4918ede2d16fdaaf82f3",
+    "url": "/Jonathons-Portfolio/static/js/2.71de0bda.chunk.js"
+  },
+  {
+    "revision": "8e1cbec9ebd37524265a",
+    "url": "/Jonathons-Portfolio/static/js/main.1f5cf5ad.chunk.js"
+  },
+  {
+    "revision": "942d2420ca802bf214cd",
+    "url": "/Jonathons-Portfolio/static/js/runtime~main.865b229d.js"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/Jonathons-Portfolio/static/media/CSS3.c26a73b4.png"
+  },
+  {
+    "revision": "74f10eaf2773996b9873f8d5a67ff639",
+    "url": "/Jonathons-Portfolio/static/media/Graduation.74f10eaf.jpg"
+  },
+  {
+    "revision": "40273454d8183b805e34faaeff2f1032",
+    "url": "/Jonathons-Portfolio/static/media/Header2.40273454.jpg"
+  },
+  {
+    "revision": "a00376e2ecdf74ba8f0fe14bbc9e9b52",
+    "url": "/Jonathons-Portfolio/static/media/NC-BE.a00376e2.png"
+  },
+  {
+    "revision": "811f7c0310afd0c7d675c4483ea303b6",
+    "url": "/Jonathons-Portfolio/static/media/NC-Photo copy.811f7c03.PNG"
+  },
+  {
+    "revision": "da18e466533a0c1ed159d5769dd5d27e",
+    "url": "/Jonathons-Portfolio/static/media/VermillionHat.da18e466.jpg"
+  },
+  {
+    "revision": "c879b2cde2777d562a63ba44c1be3489",
+    "url": "/Jonathons-Portfolio/static/media/card-holder.c879b2cd.png"
+  },
+  {
+    "revision": "1dc579918f55bcfa14cd1782d16483cd",
+    "url": "/Jonathons-Portfolio/static/media/china.1dc57991.png"
+  },
+  {
+    "revision": "18568944834decf929461bd415643e61",
+    "url": "/Jonathons-Portfolio/static/media/china2.18568944.png"
+  },
+  {
+    "revision": "2e255bee5caffac0de169d4bc3926ccd",
+    "url": "/Jonathons-Portfolio/static/media/china3.2e255bee.png"
+  },
+  {
+    "revision": "cb721b5d16a7c6e52850e6a6e2a5446f",
+    "url": "/Jonathons-Portfolio/static/media/china5.cb721b5d.png"
+  },
+  {
+    "revision": "2ee77874d864b1a911deb8553a02ab81",
+    "url": "/Jonathons-Portfolio/static/media/china6.2ee77874.png"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/Jonathons-Portfolio/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "292c4b5b4d8625c11db3297ae85445b3",
+    "url": "/Jonathons-Portfolio/static/media/dart-logo.292c4b5b.png"
+  },
+  {
+    "revision": "1e2e197c4a1f7daebc6a4ebb7cd4589c",
+    "url": "/Jonathons-Portfolio/static/media/desktop1.1e2e197c.png"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/Jonathons-Portfolio/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "2d1998a6f2729f66ea655c0fd132d8a4",
+    "url": "/Jonathons-Portfolio/static/media/flutter-logo.2d1998a6.png"
+  },
+  {
+    "revision": "00dc622157af87a42dad36d794bb0747",
+    "url": "/Jonathons-Portfolio/static/media/html5.00dc6221.png"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/Jonathons-Portfolio/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/Jonathons-Portfolio/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/Jonathons-Portfolio/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "b274aea59b3fb57a11025beda3814408",
+    "url": "/Jonathons-Portfolio/static/media/milkroom.b274aea5.png"
+  },
+  {
+    "revision": "b2eed3219c8ae0f0d1b4e3ba7a9a15c8",
+    "url": "/Jonathons-Portfolio/static/media/milkroom.b2eed321.png"
+  },
+  {
+    "revision": "3e93defae4179953517743781d4acb8a",
+    "url": "/Jonathons-Portfolio/static/media/mock3.3e93defa.gif"
+  },
+  {
+    "revision": "0246437ff5fdb9df8989d4bb0ebb8ca4",
+    "url": "/Jonathons-Portfolio/static/media/nc-news.0246437f.png"
+  },
+  {
+    "revision": "b89b83f6577baa24b0e215a198066328",
+    "url": "/Jonathons-Portfolio/static/media/nc-news2.b89b83f6.png"
+  },
+  {
+    "revision": "e0eca9c0a7d37f7a667d12e9dfc586da",
+    "url": "/Jonathons-Portfolio/static/media/ncNews-thumbnail.e0eca9c0.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/Jonathons-Portfolio/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/Jonathons-Portfolio/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/Jonathons-Portfolio/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/Jonathons-Portfolio/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/Jonathons-Portfolio/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "c353a40b9a7c7fb510ed8dc384ebb7ff",
+    "url": "/Jonathons-Portfolio/static/media/reChargeApp.c353a40b.png"
+  },
+  {
+    "revision": "04d2e216bb78bec9bacb565c2dcb209b",
+    "url": "/Jonathons-Portfolio/static/media/reChargeApp2.04d2e216.png"
+  },
+  {
+    "revision": "30c5e16db3bcb96505a4160cd0a89157",
+    "url": "/Jonathons-Portfolio/static/media/reChargeThumbnail.30c5e16d.png"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/Jonathons-Portfolio/static/media/react.638be909.png"
+  },
+  {
+    "revision": "df3f431850cac5f8f1e06bf6de0eee33",
+    "url": "/Jonathons-Portfolio/static/media/t-shirt.df3f4318.png"
+  },
+  {
+    "revision": "bf6593d503eee14d8ed8f6de75b238ea",
+    "url": "/Jonathons-Portfolio/static/media/travel.bf6593d5.png"
+  },
+  {
+    "revision": "762e8d9dce19c9693d62fdbfce112503",
+    "url": "/Jonathons-Portfolio/static/media/travel2.762e8d9d.png"
+  },
+  {
+    "revision": "3169e0c86e07e111921c04fc35267335",
+    "url": "/Jonathons-Portfolio/static/media/travel3.3169e0c8.png"
+  },
+  {
+    "revision": "402436d992d706731b2d55ab73ba220a",
+    "url": "/Jonathons-Portfolio/static/media/uni.402436d9.png"
+  },
+  {
+    "revision": "78bbd0cbd4097c7dd5385752a1c9e92c",
+    "url": "/Jonathons-Portfolio/static/media/uni1.78bbd0cb.png"
+  },
+  {
+    "revision": "83908bee181babd356591184a29ecf4a",
+    "url": "/Jonathons-Portfolio/static/media/uni2.83908bee.png"
+  },
+  {
+    "revision": "52df080c745d39f892f32d8f064bed64",
+    "url": "/Jonathons-Portfolio/static/media/uni3.52df080c.png"
+  },
+  {
+    "revision": "78ccc09425e2f183c19d41e58fdba666",
+    "url": "/Jonathons-Portfolio/static/media/venice.78ccc094.png"
+  },
+  {
+    "revision": "8ee4de9e64c2b014cc26a49509fa9224",
+    "url": "/Jonathons-Portfolio/static/media/vermillion.8ee4de9e.png"
+  }
+]);
